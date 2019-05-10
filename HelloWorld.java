05896fd5ed44f32b870c126dac283023e7e4041d
rolle003 <50482405+rolle003@users.noreply.github.com>
2019-05-10 12:59:17 +0200

    HelloWorld start

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+//CamelCase syntax in Java
+//writes a Hello message in the console window
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+		System.out.println("Hello Sumatra!");
+	
+}//main
+	
+
+}//Helloworld
